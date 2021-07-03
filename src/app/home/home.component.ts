cdd8985dc567adc0f571783ba9467ec602603931
mariam zaghouani <zaghouanim9@gmail.com>
2021-07-03 18:54:05 +0100

    almost finished home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { SwiperComponent } from "swiper/angular";
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination } from "swiper/core";
+
+// install Swiper modules
+SwiperCore.use([Pagination]);
+
+
 
 @Component({
   selector: 'app-home',
