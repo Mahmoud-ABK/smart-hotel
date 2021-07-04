b8735bcbbdc25f5bcb7abab52390a5d36535f86d
Mahmoud-ABK <benabdelkadermahmoud2003@gmail.com>
2021-07-04 17:00:40 +0100

    finished food menu

--- src/app/food-menu/food-menu.component.ts
+++ src/app/food-menu/food-menu.component.ts
@@ -16,134 +16,217 @@ SwiperCore.use([Pagination, Navigation]);
   styleUrls: ['./food-menu.component.css']
 })
 export class FoodMenuComponent implements OnInit {
-  breakfast : { name: string, pic: string }[] = [
-    {
-      name: "croissant",
-      pic: "croissant",
+  
+  breakfast: {
+    name: string, pic: string
+    , checked: boolean, label: string
+  }[] = [
+      {
+        name: "croissant",
+        pic: "croissant",
+        checked: false,
+        label: "Add to list"
 
-    },
-    {
-      name: "cake",
-      pic: "cake",
+      },
+      {
+        name: "cake",
+        pic: "cake",
+        checked: false,
+        label: "Add to list"
 
-    },
-    {
-      name: "cupcake",
-      pic: "cupcake",
-    },
+      },
+      {
+        name: "cupcake",
+        pic: "cupcake",
+        checked: false,
+        label: "Add to list"
+      },
 
-    {
-      name: "mille feuilles",
-      pic: "mille feuilles",
+      {
+        name: "mille feuilles",
+        pic: "mille feuilles",
+        checked: false,
+        label: "Add to list"
 
-    },
-    {
-      name: "pancake",
-      pic: "pancake",
-
-    },
-
-
-  ]
- drinks: { name: string, pic: string }[] = [
-    {
-      name: "chocolate smoothie",
-      pic: "chocosmoothie",
-
-    },
-    {
-      name: "milk",
-      pic: "milk",
-
-    },
-    {
-      name: "coffee",
-      pic: "coffee",
-    },
-
-    {
-      name: "water",
-      pic: "water",
-
-    },
-    {
-      name: "kiwi coctail",
-      pic: "kiwi coctail",
-
-    },
-  ]
-  lunch: { name: string, pic: string }[] = [
-    {
-      name: "couscous",
-      pic: "couscous",
-
-    },
-    {
-      name: "salad",
-      pic: "salad",
-
-    },
-    {
-      name: "pizza",
-      pic: "pizza",
-    },
-
-    {
-      name: "rice",
-      pic: "rice",
-
-    },
-    {
-      name: "spaghetti",
-      pic: "spaghetti",
-
-    },
+      },
+      {
+        name: "pancake",
+        pic: "pancake",
+        checked: false,
+        label: "Add to list"
 
 
-  ]
-  dinner: { name: string, pic: string }[] = [
-    {
-      name: "mtabga",
-      pic: "mtabga",
-
-    },
-    {
-      name: "salmon",
-      pic: "salmon",
-
-    },
-    {
-      name: "soup",
-      pic: "soup",
-    },
+      },
 
 
+    ]
+  drinks: {
+    name: string, pic: string
+    , checked: boolean, label: string
+  }[] = [
+      {
+        name: "chocolate smoothie",
+        pic: "chocosmoothie",
+        checked: false,
+        label: "Add to list"
 
-  ]
-  snacks: { name: string, pic: string }[] = [
-    {
-      name: "Raspberries",
-      pic: "raspberries",
+      },
+      {
+        name: "milk",
+        pic: "milk",
+        checked: false,
+        label: "Add to list"
 
-    },
-    {
-      name: "yogurt",
-      pic: "yogurt",
+      },
+      {
+        name: "coffee",
+        pic: "coffee",
+        checked: false,
+        label: "Add to list"
+      },
 
-    },
-    {
-      name: "ice cream",
-      pic: "ice cream",
-    },
+      {
+        name: "water",
+        pic: "water",
+        checked: false,
+        label: "Add to list"
+
+      },
+      {
+        name: "kiwi coctail",
+        pic: "kiwi coctail",
+        checked: false,
+        label: "Add to list"
+
+      },
+    ]
+  lunch: {
+    name: string, pic: string
+    , checked: boolean, label: string
+  }[] = [
+      {
+        name: "couscous",
+        pic: "couscous",
+        checked: false,
+        label: "Add to list"
+
+      },
+      {
+        name: "salad",
+        pic: "salad",
+        checked: false,
+        label: "Add to list"
+
+      },
+      {
+        name: "pizza",
+        pic: "pizza",
+        checked: false,
+        label: "Add to list"
+      },
+
+      {
+        name: "rice",
+        pic: "rice",
+        checked: false,
+        label: "Add to list"
+
+      },
+      {
+        name: "spaghetti",
+        pic: "spaghetti",
+        checked: false,
+        label: "Add to list"
+
+      },
 
 
+    ]
+  dinner: {
+    name: string, pic: string
+    , checked: boolean, label: string
+  }[] = [
+      {
+        name: "mtabga",
+        pic: "mtabga",
+        checked: false,
+        label: "Add to list"
+
+      },
+      {
+        name: "salmon",
+        pic: "salmon",
+        checked: false,
+        label: "Add to list"
+
+      },
+      {
+        name: "soup",
+        pic: "soup",
+        checked: false,
+        label: "Add to list"
+      },
+
+
+
+    ]
+  snacks: {
+    name: string, pic: string
+    , checked: boolean, label: string
+  }[] = [
+      {
+        name: "Raspberries",
+        pic: "raspberries",
+        checked: false,
+        label: "Add to list"
+
+      },
+      {
+        name: "yogurt",
+        pic: "yogurt",
+        checked: false,
+        label: "Add to list"
+
+      },
+      {
+        name: "ice cream",
+        pic: "ice cream",
+        checked: false,
+        label: "Add to list"
+      },
+
+
+
+    ]
+  list: string[] = [
 
   ]
   constructor() { }
 
   ngOnInit(): void {
   }
-  
+  removerFromList(a:string) {
+    this.list.forEach((name, index) => {
+      if (name === a) this.list.splice(index, 1);
+    });
+  }
+  onChecked(item) {
+    item.checked = !item.checked
+    console.log(item.checked)
+    if (item.checked) {
+      this.list.push(item.name)
+      console.log(this.list);
+
+      item.label = "Remove from list"
+    }
+    else {
+      item.label = "Add to list"
+      this.removerFromList(item.name)
+      console.log(this.list)
+
+    }
+
+  }
 
 
 
