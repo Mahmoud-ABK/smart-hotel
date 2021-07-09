2de1e4492e291b62d4c64ecba42acfe412bf1694
mariam zaghouani <zaghouanim9@gmail.com>
2021-07-09 22:53:01 +0100

    finished manager interface and doing kitchen page

--- src/app/staff-pages/kitchen-page/kitchen-page.component.ts
+++ src/app/staff-pages/kitchen-page/kitchen-page.component.ts
@@ -7,6 +7,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class KitchenPageComponent implements OnInit {
 
+  orders:Array<object>=[
+    {
+      roomID:1,
+      roomOwner:"khalid",
+    },
+    {
+      roomID:2,
+      roomOwner:"bou khalid",
+    },
+    {
+      roomID:3,
+      roomOwner:"aamet khalid",
+    },
+  ]
+
+  foodList:Array<object>=[
+    {
+      dishName: [
+        'coscous',
+        'lasagne',
+      ],
+      quantity: [
+        2,
+        3,
+        ],
+    },
+    {
+      dishName: [
+        'meal-1',
+        'meal-2',
+        'meal-3',
+      ],
+      quantity: [
+        2,
+        3,
+        1
+        ],
+    },
+    {
+      dishName: [
+        'ma9rouna',
+        'kaskrout',
+        'coscous',
+        'lasagne',
+      ],
+      quantity: [
+        1,
+        3,
+        5,
+        6
+        ],
+    },
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
