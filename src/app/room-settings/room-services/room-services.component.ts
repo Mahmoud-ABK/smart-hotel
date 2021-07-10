e7782671d77c2a10074869b384ad3859dd4da26e
Arij-Hamouda <80691535+Arij-Hamouda@users.noreply.github.com>
2021-07-10 21:23:58 +0100

    room services form done

--- src/app/room-settings/room-services/room-services.component.ts
+++ src/app/room-settings/room-services/room-services.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-room-services',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-services.component.css']
 })
 export class RoomServicesComponent implements OnInit {
-disable:boolean=false
+disable:boolean
+
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onSubmit(form: NgForm){
+    console.log(form.value.cleaningDateTime);
+    console.log("hey")
+  }
 }
