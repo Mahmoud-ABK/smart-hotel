9986028df998451820e69c1a93e2d1983bc8c71d
Arij-Hamouda <80691535+Arij-Hamouda@users.noreply.github.com>
2021-07-11 00:02:16 +0100

    forms of report , room control and rooms services

--- src/app/room-settings/room-control/room-control.component.ts
+++ src/app/room-settings/room-control/room-control.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-room-control',
@@ -12,11 +13,46 @@ export class RoomControlComponent implements OnInit {
    c:true,
 
  }
+ lights={
+   a:false,
+   b:false,
+   c:false,
+   d:false
+ }
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
-
+  onSubmit(form: NgForm){
+    console.log("hey")
+    console.log(this.undefinedtem(form.value.tempvalue));
+    console.log(this.undefinedhum(form.value.hvalue));
+    console.log(this.lights.a);
+    console.log(form.value.lightswitch2);
+    console.log(form.value.lightswitch3);
+    console.log(form.value.lightswitch4);
+    console.log(this.undefinedwatertemp(form.value.watertempvalue));
+  }
+  undefinedtem(formcontrol:any){
+if (formcontrol==undefined){
+  return 25
+}else{
+  return formcontrol
+}
+  }
+  undefinedhum(formcontrol:any){
+    if (formcontrol==undefined){
+      return 30
+    }else{
+      return formcontrol
+    }
+      }
+      undefinedwatertemp(formcontrol:any){
+        if (formcontrol==undefined){
+          return 70
+        }else{
+          return formcontrol
+        }
+          }
 }
--- src/app/room-settings/room-report/room-report.component.ts
+++ src/app/room-settings/room-report/room-report.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-room-report',
   templateUrl: './room-report.component.html',
@@ -11,5 +11,9 @@ export class RoomReportComponent implements OnInit {
 
   ngOnInit(): void {
   }
-
+  onSubmit(form: NgForm){
+    console.log(form.value.problemTitle);
+    console.log(form.value.problemDesciption);
+    console.log(form.value.formFileLg);
+  }
 }
