c78d6db12c9061ca63496279b888c52fdbb96e07
mariam zaghouani <zaghouanim9@gmail.com>
2021-07-10 22:19:43 +0100

    login manager page form done

--- src/app/login-pages/login-manager/login-manager.component.ts
+++ src/app/login-pages/login-manager/login-manager.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login-manager',
@@ -10,6 +11,12 @@ export class LoginManagerComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  
+  onSubmit(form: NgForm){
+    console.log(form.value.email);
+    console.log(form.value.managerpsw);
   }
 
 }
