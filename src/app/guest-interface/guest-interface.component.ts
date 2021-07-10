aedaedd657ca11f1ad6235e19b21f0f0a8f8df50
Arij-Hamouda <80691535+Arij-Hamouda@users.noreply.github.com>
2021-07-10 12:15:38 +0100

    styling guest interface

--- src/app/guest-interface/guest-interface.component.ts
+++ src/app/guest-interface/guest-interface.component.ts
@@ -6,7 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./guest-interface.component.css']
 })
 export class GuestInterfaceComponent implements OnInit {
-
+rooms:Array<object>=[
+  {
+    roomID:3,
+    checkoutDate:"33/33/3333"  },
+  {
+    roomID:7,
+    checkoutDate:"6/63/3338"  },
+  {
+      roomID:9,
+      checkoutDate:"33/33/3733"}
+]
   constructor() { }
 
   ngOnInit(): void {
