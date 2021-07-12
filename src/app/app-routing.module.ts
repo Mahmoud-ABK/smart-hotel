e44f9a915870457871f340fbe26160287ab2d557
Arij-Hamouda <80691535+Arij-Hamouda@users.noreply.github.com>
2021-07-12 23:18:28 +0100

    routing done (without guard):login pages managerinterface,staff pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -13,11 +13,16 @@ import { LoginGuestComponent } from './login-pages/login-guest/login-guest.compo
 import { LoginManagerComponent } from './login-pages/login-manager/login-manager.component';
 import { LoginRoundaboutComponent } from './login-pages/login-roundabout/login-roundabout.component';
 import { LoginServantPageComponent } from './login-pages/login-servant-page/login-servant-page.component';
+import { ManagerInterfaceComponent } from './manager-interface/manager-interface.component';
+import { RoomManagementComponent } from './manager-interface/room-management/room-management.component';
+import { RoomReportMenuComponent } from './manager-interface/room-report-menu/room-report-menu.component';
 import { RoomControlComponent } from './room-settings/room-control/room-control.component';
 import { RoomReportComponent } from './room-settings/room-report/room-report.component';
 import { RoomSecurityComponent } from './room-settings/room-security/room-security.component';
 import { RoomServicesComponent } from './room-settings/room-services/room-services.component';
 import { RoomSettingsComponent } from './room-settings/room-settings.component';
+import { KitchenPageComponent } from './staff-pages/kitchen-page/kitchen-page.component';
+import { ServantPageComponent } from './staff-pages/servant-page/servant-page.component';
 
 
 const routes: Routes = [
@@ -29,6 +34,23 @@ const routes: Routes = [
   {path:"kitchenlogin",component:KitchenPageLoginComponent},
   {path:"servantlogin",component:LoginServantPageComponent},
   {path:"guestinterface",component:GuestInterfaceComponent},
+  {path:"servantpage",component:ServantPageComponent},
+  {path:"kitchenpage",component:KitchenPageComponent},
+  {
+    path:"managerinterface",
+    component:ManagerInterfaceComponent,
+    children:[
+      {
+        path:"roommanagement",
+        component:RoomManagementComponent
+      },
+      {
+        path:"roomreportmenu",
+        component:RoomReportMenuComponent
+      }
+    ]
+  
+  },
   {
     path:"roomsettings",
     component:RoomSettingsComponent,
