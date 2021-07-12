3561d450941fc0c8e87619a405c5dab799ed9752
Mahmoud-ABK <benabdelkadermahmoud2003@gmail.com>
2021-07-12 14:20:49 +0100

    added firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false , 
+  firebase : {
+    apiKey: "AIzaSyBarjZHURqowdsxgN4PyJbvI8mjifWYX5w",
+    authDomain: "smarthotel-database.firebaseapp.com",
+    databaseURL: "https://smarthotel-database-default-rtdb.firebaseio.com",
+    projectId: "smarthotel-database",
+    storageBucket: "smarthotel-database.appspot.com",
+    messagingSenderId: "964675175650",
+    appId: "1:964675175650:web:c85a5b6ff4471cfa504abb",
+    measurementId: "G-B4GVGF6DYL"
+  }
 };
 
 /*
